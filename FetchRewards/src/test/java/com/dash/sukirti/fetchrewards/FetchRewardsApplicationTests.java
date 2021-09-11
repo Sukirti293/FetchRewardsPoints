d4a33214103c5c58987d6d1a221fb24454411a04
Sukirti293 <sukirtidash1993@gmail.com>
2021-09-11 02:23:46 -0400

    #1

---
+++ FetchRewards/src/test/java/com/dash/sukirti/fetchrewards/FetchRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dash.sukirti.fetchrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchRewardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
